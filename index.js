b7e930814c6a024cf1567718d9b60c693a7fc1a0
ArthMcFly <102303972+ArthMcFly@users.noreply.github.com>
2023-02-21 19:42:02 +0000

    criação do chat criação do chat (errado)

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require('express');
+const path = require ('path');
+
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const server = require ('http').createServer(app);
+const io = require ('socket.io')(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view.engine','html');
+
+app.use('/', (req,res) => {
+    res.render('index.html');
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
-
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+server.listen(8080);
