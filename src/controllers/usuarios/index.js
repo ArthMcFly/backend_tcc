1232071bd3d68db560f5176cb9d10c936fb4b99a
Arthur <arthurgomesferreira.agf@gmail.com>
2023-03-30 18:39:00 -0300

    New routes addded

--- src/controllers/usuarios/index.js
+++ src/controllers/usuarios/index.js
@@ -1,12 +1,22 @@
-class users {
+class Users {
 	async store(req, res, next) {
-		req.body = {...req.body, created_at: new Date(), updated_at: ""};
+		!req.body?.usu_nivel && (req.body = {...req.body, usu_nivel:"aluno"});
+		!req.body?.usu_bio && (req.body = {...req.body, usu_bio:""});
+		req.body = {
+			...req.body,
+			usu_fotoperfil: "",
+			created_at: new Date(),
+			updated_at: ""
+		};
 		next();
 	}
 	async update(req, res, next) {
-		req.body = {...req.body, updated_at: new Date()};
+		req.body = {
+			...req.body,
+			updated_at: new Date()
+		};
 		next();
 	}
 }
 
-module.exports = new users();
+module.exports = new Users();
